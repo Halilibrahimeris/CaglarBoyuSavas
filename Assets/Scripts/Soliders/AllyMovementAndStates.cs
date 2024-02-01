6d399f4a4986016435c14a1681483eaa6413038b
zassamand <halilibrahimeris3@gmail.com>
2024-02-01 13:20:27 +0300

    Update
    Karakter sistemi düzeltildi savaş ile alakalı bazı buglar giderildi

--- Assets/Scripts/Soliders/AllyMovementAndStates.cs
+++ Assets/Scripts/Soliders/AllyMovementAndStates.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class AllyMovementAndStates : MonoBehaviour
 {
-    List<string> SoliderType = new List<string> { "Ally", "Enemy" };//Oyundaki Karakter Tipleri
+    public List<string> SoliderType = new List<string> { "Ally", "Enemy" };//Oyundaki Karakter Tipleri
     public string SoliderTypeS;//Karakter Tiplerinin bulunduðu string
 
     Attack attack; //Attack Classý
@@ -110,80 +110,5 @@ public class AllyMovementAndStates : MonoBehaviour
             Anim.SetBool("isRunning", true);
         }//Yürüme durumuna devam edecek
     }
-    private void OnTriggerEnter(Collider other)//Karakterde bulunan collidera bir nesne girer ise
-    {
-        if (SoliderTypeS == SoliderType[0])//Asker tipi Ally ise
-        {
-            if (other.CompareTag("Enemy"))//Giren nesne Enemy ise
-            {
-                if (Enemy == null)// Aktif bir düþman yok ise
-                {
-                    Enemy = other.gameObject;//Giren nesne düþman olacak
-                    Attack = true;//attack durumuna geçilecek
-                    Run = false;//koþma durumu kapalý olacak
-                    Anim.SetBool("isRunning", false);//animasyon koþma durumu kapatýlacak
-                }
-            }
-        }
-        else//ASker tipi Enemy ise
-        {
-            if (other.CompareTag("Ally"))//Giren nesne Ally ise
-            {
-                if (Enemy == null)// Aktif bir düþman yok ise
-                {
-                    Enemy = other.gameObject;//Giren nesne düþman olacak
-                    Attack = true;//attack durumuna geçilecek
-                    Run = false;//koþma durumu kapalý olacak
-                    Anim.SetBool("isRunning", false);//animasyon koþma durumu kapatýlacak
-                }
-            }
-        }
-    }
-
-    private void OnTriggerStay(Collider other)//Karakterde bulunan collidera bir nesne duruyor ise
-    {
-        if (SoliderTypeS == SoliderType[0])//Asker tipi Ally ise
-        {
-            if (other.CompareTag("Enemy"))//Duran nesne Enemy ise
-            {
-                Attack = true;//attack durumunda duracak
-                Run = false;//koþma durumu kapalý kalacak
-                Anim.SetBool("isRunning", false);//animasyon koþma durumu kapalý kalacak
-            }
-        }
-        else//Asker tipi Enemy ise
-        {
-            if (other.CompareTag("Ally"))//Duran nesne Ally ise
-            {
-                Attack = true;//attack durumunda duracak
-                Run = false;//koþma durumu kapalý kalacak
-                Anim.SetBool("isRunning", false);//animasyon koþma durumu kapalý kalacak
-            }
-        }
-          
-    }
-    private void OnTriggerExit(Collider other) //Karakterde bulunan colliderdan bir nesne çýktý ise
-    {
-        if(SoliderTypeS == SoliderType[0])  //Asker tipi Ally ise
-        {
-            if (other.CompareTag("Enemy"))  //Çýkan nesne Enemy ise
-            {
-                Attack = false;                  //Attack durumu kapatýlacak
-                Enemy = null;                    //Enemy olmadýðý kabul edilecek
-                Run = true;                      //Yürüme durumu baþlayacak
-                Anim.SetBool("isRunning", true); //Yürüme animasyonu baþlatýlacak
-            }
-        }
-        else                               //Asker tipi Enemy ise
-        {
-            if (other.CompareTag("Ally"))  //Çýkan nesne Ally ise
-            {
-                Attack = false;                  //Attack durumu kapatýlacak
-                Enemy = null;                    //Enemy olmadýðý kabul edilecek
-                Run = true;                      //Yürüme durumu baþlayacak
-                Anim.SetBool("isRunning", true); //Yürüme animasyonu baþlatýlacak
-            }
-        }
-        
-    }
+    
 }
---
+++ Assets/Scripts/Soliders/Triggers.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triggers : MonoBehaviour
+{
+    AllyMovementAndStates states;
+    private void Start()
+    {
+        states = GetComponentInParent<AllyMovementAndStates>();
+    }
+
+    private void OnTriggerEnter(Collider other)//Karakterde bulunan collidera bir nesne girer ise
+    {
+        if (states.SoliderTypeS == states.SoliderType[0])//Asker tipi Ally ise
+        {
+            if (other.CompareTag("Enemy"))//Giren nesne Enemy ise
+            {
+                if (states.Enemy == null)// Aktif bir düþman yok ise
+                {
+                    states.Enemy  = other.gameObject;//Giren nesne düþman olacak
+                    states.Attack = true;//attack durumuna geçilecek
+                    states.Run = false;//koþma durumu kapalý olacak
+                    states.Anim.SetBool("isRunning", false);//animasyon koþma durumu kapatýlacak
+                }
+            }
+        }
+        else//ASker tipi Enemy ise
+        {
+            if (other.CompareTag("Ally"))//Giren nesne Ally ise
+            {
+                if (states.Enemy == null)// Aktif bir düþman yok ise
+                {
+                    states.Enemy = other.gameObject;//Giren nesne düþman olacak
+                    states.Attack = true;//attack durumuna geçilecek
+                    states.Run = false;//koþma durumu kapalý olacak
+                    states.Anim.SetBool("isRunning", false);//animasyon koþma durumu kapatýlacak
+                }
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)//Karakterde bulunan collidera bir nesne duruyor ise
+    {
+        if (states.SoliderTypeS == states.SoliderType[0])//Asker tipi Ally ise
+        {
+            if (other.CompareTag("Enemy"))//Duran nesne Enemy ise
+            {
+                states.Attack = true;//attack durumunda duracak
+                states.Run = false;//koþma durumu kapalý kalacak
+                states.Anim.SetBool("isRunning", false);//animasyon koþma durumu kapalý kalacak
+            }
+        }
+        else//Asker tipi Enemy ise
+        {
+            if (other.CompareTag("Ally"))//Duran nesne Ally ise
+            {
+                states.Attack = true;//attack durumunda duracak
+                states.Run = false;//koþma durumu kapalý kalacak
+                states.Anim.SetBool("isRunning", false);//animasyon koþma durumu kapalý kalacak
+            }
+        }
+
+    }
+    private void OnTriggerExit(Collider other) //Karakterde bulunan colliderdan bir nesne çýktý ise
+    {
+        if (states.SoliderTypeS == states.SoliderType[0])  //Asker tipi Ally ise
+        {
+            if (other.CompareTag("Enemy"))  //Çýkan nesne Enemy ise
+            {
+                states.Attack = false;                  //Attack durumu kapatýlacak
+                states.Enemy = null;                    //Enemy olmadýðý kabul edilecek
+                states.Run = true;                      //Yürüme durumu baþlayacak
+                states.Anim.SetBool("isRunning", true); //Yürüme animasyonu baþlatýlacak
+            }
+        }
+        else                               //Asker tipi Enemy ise
+        {
+            if (other.CompareTag("Ally"))  //Çýkan nesne Ally ise
+            {
+                states.Attack = false;                  //Attack durumu kapatýlacak
+                states.Enemy = null;                    //Enemy olmadýðý kabul edilecek
+                states.Run = true;                      //Yürüme durumu baþlayacak
+                states.Anim.SetBool("isRunning", true); //Yürüme animasyonu baþlatýlacak
+            }
+        }
+
+    }
+}
